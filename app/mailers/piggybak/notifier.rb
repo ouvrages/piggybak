4d8599b681d12cdc92da612c0070bb0d82e7aa8c
Michael Witrant <michael@ouvrages-web.fr>
2012-06-26 14:52:52 +0200

    get email subject from the locale

--- app/mailers/piggybak/notifier.rb
+++ app/mailers/piggybak/notifier.rb
@@ -6,8 +6,7 @@ module Piggybak
     def order_notification(order)
       @order = order
 
-      mail(:to => order.email,
-           :subject => "Order ##{@order.id}")
+      mail(:to => order.email)
     end
   end
 end
