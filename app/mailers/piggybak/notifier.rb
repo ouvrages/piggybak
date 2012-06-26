e5930474c125fd87898e2a5f1bd01dbafc9bb5c2
Michael Witrant <michael@ouvrages-web.fr>
2012-06-26 16:26:55 +0200

    set locale before sending email

--- app/mailers/piggybak/notifier.rb
+++ app/mailers/piggybak/notifier.rb
@@ -6,7 +6,15 @@ module Piggybak
     def order_notification(order)
       @order = order
 
-      mail(:to => order.email)
+      begin
+        if @order.respond_to? :locale
+          initial_locale = I18n.locale
+          I18n.locale = @order.locale || :en
+        end
+        mail(:to => order.email)
+      ensure
+        I18n.locale = initial_locale if initial_locale
+      end
     end
     
     def staff_notification(order)
