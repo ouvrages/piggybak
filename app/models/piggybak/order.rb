173ee17e30a769beaa1acc4e992e3386eb9d47e5
Michael Witrant <michael@ouvrages-web.fr>
2012-06-28 16:46:28 +0200

    removed notifier (should use an observer instead)

--- app/models/piggybak/order.rb
+++ app/models/piggybak/order.rb
@@ -26,7 +26,6 @@ module Piggybak
     before_validation :set_defaults
     after_validation :update_totals
     before_save :process_payments, :update_status
-    after_save :send_notifications
 
     attr_accessible :email, :phone, :billing_address_attributes, :shipping_address_attributes, :payments_attributes, :shipments_attributes
     
@@ -164,13 +163,6 @@ module Piggybak
 
     def subtotal
       self.line_items.inject(0) { |subtotal, li| subtotal + li.total }
-    end
-    
-    def send_notifications
-      if status_changed? and changes["status"].last == "paid"
-        Piggybak::Notifier.order_notification(self).deliver
-        Piggybak::Notifier.staff_notification(self).deliver if Piggybak.config.staff_notification_email
-      end
     end
     
     def total_shipments
--- lib/piggybak/config.rb
+++ lib/piggybak/config.rb
@@ -6,15 +6,9 @@ module Piggybak
       attr_accessor :tax_calculators
       attr_accessor :default_country
       attr_accessor :activemerchant_mode
-      attr_accessor :email_sender
-      attr_accessor :order_cc
       attr_accessor :payment_flow
-      attr_accessor :staff_notification_email, :staff_locale
 
       def reset
-        @email_sender = "support@piggybak.org"
-        @order_cc = nil
-
         @payment_calculators = ["::Piggybak::PaymentCalculator::Fake",
                                 "::Piggybak::PaymentCalculator::AuthorizeNet"]
         @shipping_calculators = ["::Piggybak::ShippingCalculator::FlatRate",
@@ -27,9 +21,6 @@ module Piggybak
         @activemerchant_mode = :production
         
         @payment_flow = :gateway
-        
-        @staff_notification_email = nil
-        @staff_locale = nil
       end
     end
 
